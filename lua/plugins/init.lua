124ee25c953976e558102a576067a79ab982860f
dqnid <dani.heras@hotmail.com>
2024-11-10 22:42:03 +0100

    feat(obsidian): obsidian support added in order to transition from UI to nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -466,6 +466,27 @@ return {
         end,
     },
 
+    -- ╭──────────╮
+    -- │ Obsidian │
+    -- ╰──────────╯
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "default",
+                    path = "~/Remote/Cloud/Documents/notes/md",
+                },
+            },
+        },
+    },
+
     -- OPTIONAL --
     --------------
     -- Mini map
