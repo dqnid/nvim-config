02597affa58362c54005e0968021bf9a52427f68
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-05-05 15:41:25 +0200

    feat: smear adjustments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -401,7 +401,21 @@ return {
 
     {
         "sphamba/smear-cursor.nvim",
-        opts = {},
+        opts = {
+            smear_between_buffers = false,
+            smear_between_neighbor_lines = true,
+            scroll_buffer_space = true,
+            -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+            legacy_computing_symbols_support = false,
+            smear_insert_mode = true,
+
+            -- fast smear
+            stiffness = 0.8, -- 0.6      [0, 1]
+            trailing_stiffness = 0.5, -- 0.4      [0, 1]
+            stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+            trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+            distance_stop_animating = 0.5, -- 0.1      > 0
+        },
     },
 
     -- ╭───────────────╮
