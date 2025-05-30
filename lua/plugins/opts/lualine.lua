e292595af374a6b9058a6158efcb990ad9d1ed2f
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-05-30 15:32:44 +0200

    style: new line icons

--- lua/plugins/opts/lualine.lua
+++ lua/plugins/opts/lualine.lua
@@ -2,14 +2,17 @@ local options = {
     options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        component_separators = { left = "╎", right = "╎" },
+        section_separators = { left = "┃", right = "┃" },
         disabled_filetypes = {
             statusline = {},
             winbar = {},
         },
         ignore_focus = {},
         always_divide_middle = true,
+        always_show_tabline = true,
         globalstatus = true,
         refresh = {
             statusline = 1000,
