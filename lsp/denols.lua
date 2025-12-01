269cbc23ea5c95c8fe665c3bb0c299729df16691
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-12-01 20:01:23 +0100

    fix: deno + ts_ls

--- lsp/denols.lua
+++ lsp/denols.lua
@@ -2,4 +2,5 @@ return {
 	cmd = { 'deno', 'lsp' },
 	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 	root_markers = { "deno.json", "deno.jsonc" },
+	workspace_required = true,
 }
--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,5 +1,7 @@
 return {
-    cmd = { 'typescript-language-server', '--stdio' },
-    filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript', 'javascriptreact', 'javascript.jsx', 'js', 'jsx', 'ts','tsx' },
-    root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+	cmd = { 'typescript-language-server', '--stdio' },
+	filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript', 'javascriptreact', 'javascript.jsx', 'js', 'jsx', 'ts', 'tsx' },
+	root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json' },
+	single_file_support = false,
+	workspace_required = true,
 }
